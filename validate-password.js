3b6ea5878676044392ab1c8dca2275140512162f
Chris <167183247+demodrome@users.noreply.github.com>
2024-05-21 17:19:35 +0100

    feat: Add comment to validator

--- validate-password.js
+++ validate-password.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('[data-form]');
 const passwordFields = document.querySelectorAll('input[type="password"]');
 
+// Check for matching passwords when confirm password field loses focus
 passwordFields[1].addEventListener('focusout', e => {
   if (passwordFields[0].value !== passwordFields[1].value) {
     passwordFields.forEach(passwordField => {
